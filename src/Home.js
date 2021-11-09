1f1d097997239fbeb0c339cebef0885c0daf1b41
Pratyush Thota <pratyushthota@Pratyushs-MacBook-Pro.local>
2021-11-08 22:05:48 -0700

    updated

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,7 @@ function Home(){
             <div style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap', flexDirection:'row-reverse'}}>
                 <HomeIcon style={{width:'7vh',height:'7vh'}} onClick={handleClick}/>
             </div>
-            <h1>WELCOME</h1> 
+            <h1 style={{color:'grey'}}>WELCOME</h1> 
             <h4>What would you like to do today?</h4>
             <Button onClick={goToStart} style={{color:'#1e90ff'}} variant='outlined'>Continue from where you left off</Button><br/><br/>
             <Button style={{color:'grey'}} variant='outlined' disabled>Start new challenge </Button> 
